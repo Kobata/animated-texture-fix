aa5a6737b18b84a923107fec1317a096113d1aed
Kobata <kobata2@gmail.com>
2015-05-09 22:29:10 -0400

    Add option to skip smaller mip levels in animation updates

--- src/main/java/animfix/AnimfixModContainer.java
+++ src/main/java/animfix/AnimfixModContainer.java
@@ -7,6 +7,7 @@ import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
+import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.ContextCapabilities;
 import org.lwjgl.opengl.GLContext;
 
@@ -16,13 +17,15 @@ import java.util.Arrays;
 public class AnimfixModContainer extends DummyModContainer {
     public static boolean copyImageSupported = false;
     public static boolean copyImageEnabled = false;
+    public static int maxUpdateMip = -1;
+    public static Logger log = null;
 
     public AnimfixModContainer() {
         super(new ModMetadata());
         ModMetadata meta = getMetadata();
         meta.modId="animfix";
         meta.name="Animated Texture Fix";
-        meta.version="0.2";
+        meta.version="0.3";
         meta.authorList= Arrays.asList("Kobata");
         meta.description="Makes animated textures faster.";
     }
@@ -35,18 +38,21 @@ public class AnimfixModContainer extends DummyModContainer {
 
     @Subscribe
     public void preInit(FMLPreInitializationEvent evt) {
+        log = evt.getModLog();
+
         ContextCapabilities caps = GLContext.getCapabilities();
         copyImageSupported = caps.OpenGL43 || caps.GL_ARB_copy_image;
         if(!copyImageSupported) {
-            evt.getModLog().warn("Fast animated textures require OpenGL 4.3 or ARB_copy_image extension, which were not detected. Using original slow path.");
+            log.warn("Fast animated textures require OpenGL 4.3 or ARB_copy_image extension, which were not detected. Using original slow path.");
         } else {
-            evt.getModLog().info("Using fast animated textures.");
+            log.info("Using fast animated textures.");
         }
 
         File configFile = evt.getSuggestedConfigurationFile();
         Configuration config = new Configuration(configFile);
 
         boolean enableFastAnimation = config.getBoolean("enableFastAnimation", "animfix", true, "Enable the faster animation mode. Set to false only if true causes issues.");
+        maxUpdateMip = config.getInt("maxUpdateMipLevel", "animfix", -1, -1, 4, "Set to a number to disable animation updates past that mip level. -1 means update all. Higher numbers update more levels.");
 
         if(config.hasChanged()) {
             config.save();
--- src/main/java/animfix/FastTextureAtlasSprite.java
+++ src/main/java/animfix/FastTextureAtlasSprite.java
@@ -6,8 +6,9 @@ import org.lwjgl.opengl.ARBCopyImage;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL14;
-import org.lwjgl.opengl.GL43;
+import org.lwjgl.util.glu.GLU;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class FastTextureAtlasSprite extends TextureAtlasSprite {
@@ -31,24 +32,29 @@ public class FastTextureAtlasSprite extends TextureAtlasSprite {
             int k = animationMetadata.getFrameIndex(frameCounter);
 
             if (i != k && k >= 0 && k < framesTextureData.size()) {
-                int[][] frameData = (int[][])this.framesTextureData.get(k);
 
                 if(AnimfixModContainer.copyImageEnabled && textureId != -1) {
                     int destTex = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+                    checkGLError("updateAnimation | fastPath getPreviousTexture");
 
                     // Unbinding texture for safety, since copy image has an explicit destination.
                     GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+                    checkGLError("updateAnimation | fastPath unbindTex");
 
-                    for (int mip = 0; mip < frameData.length; ++mip) {
+                    for (int mip = 0; mip <= mipLevels; ++mip) {
                         ARBCopyImage
                                 .glCopyImageSubData(textureId, GL11.GL_TEXTURE_2D, mip, (width * k) >> mip, 0, 0, destTex, GL11.GL_TEXTURE_2D, mip, originX >> mip,
                                                     originY >> mip,
                                                     0, width >> mip, height >> mip, 1);
+                        checkGLError("updateAnimation | fastPath mip="+mip);
                     }
 
                     GL11.glBindTexture(GL11.GL_TEXTURE_2D, destTex);
+                    checkGLError("updateAnimation | fastPath rebindTex");
                 } else {
-                    TextureUtil.uploadTextureMipmap(frameData, width, height, originX, originY, false, false);
+                    int[][] frameData = (int[][])this.framesTextureData.get(k);
+                    uploadTextureMaxMips(mipLevels, frameData, width, height, originX, originY, false, false, frameData.length > 1);
+                    checkGLError("updateAnimation | slowPath");
                 }
             }
         }
@@ -59,6 +65,7 @@ public class FastTextureAtlasSprite extends TextureAtlasSprite {
         if(textureId != -1) {
             GL11.glDeleteTextures(textureId);
             textureId = -1;
+            checkGLError("setFramesTextureData | deleteTexture");
         }
 
         super.setFramesTextureData(p_110968_1_);
@@ -66,32 +73,63 @@ public class FastTextureAtlasSprite extends TextureAtlasSprite {
         // No need for extra texture if there's only one frame.
         if(p_110968_1_.size() > 1 && AnimfixModContainer.copyImageEnabled) {
             textureId = GL11.glGenTextures();
+            checkGLError("setFramesTextureData | createTexture");
 
             int prevTex = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+            checkGLError("setFramesTextureData | getPreviousTexture");
 
             // Set up holding texture
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
+            checkGLError("setFramesTextureData | bindTexture");
 
             if (mipLevels > 0) {
                 GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LEVEL, mipLevels);
+                checkGLError("setFramesTextureData | setTextureProperties[Mip Levels]");
                 GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_LOD, 0.0F);
+                checkGLError("setFramesTextureData | setTextureProperties[LOD Min]");
                 GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LOD, (float) mipLevels);
+                checkGLError("setFramesTextureData | setTextureProperties[LOD Max]");
                 GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0.0F);
+                checkGLError("setFramesTextureData | setTextureProperties[LOD Bias]");
             }
 
             // Reserve memory for texture
             for (int i = 0; i <= mipLevels; ++i) {
                 GL11.glTexImage2D(GL11.GL_TEXTURE_2D, i, GL11.GL_RGBA, (width * framesTextureData.size()) >> i, height >> i, 0, GL12.GL_BGRA,
                                   GL11.GL_UNSIGNED_BYTE, (java.nio.ByteBuffer) null);
+                checkGLError("setFramesTextureData | createMip " + i);
             }
 
             // Copy animation frames to holding texture
             for (int i = 0; i < framesTextureData.size(); ++i) {
-                TextureUtil.uploadTextureMipmap((int[][]) framesTextureData.get(i), width, height, width * i, 0, false, false);
+                uploadTextureMaxMips(mipLevels, (int[][])framesTextureData.get(i), width, height, width * i, 0, false, false, mipLevels > 0);
+                checkGLError("setFramesTextureData | uploadFrame " + i);
             }
 
             // Restore old texture
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, prevTex);
+            checkGLError("setFramesTextureData | restoreTexture");
+        }
+    }
+
+    private static void uploadTextureMaxMips(int maxMips, int[][] data, int width, int height, int originX, int originY, boolean linearFiltering, boolean clamped, boolean mipFiltering) {
+        int mips = maxMips >= 0 ? Math.min(maxMips, data.length-1) : data.length-1;
+        for (int mip = 0; mip <= mips; ++mip) {
+            TextureUtil.uploadTextureSub(mip, data[mip], width >> mip, height >> mip, originX >> mip, originY >> mip, linearFiltering,
+                                         clamped, mipFiltering);
+            checkGLError("uploadTextureMaxMips mip="+mip);
+        }
+    }
+
+    private static void checkGLError(String desc) {
+        int error = GL11.glGetError();
+
+        while(error != GL11.GL_NO_ERROR) {
+            String errorString = GLU.gluErrorString(error);
+
+            AnimfixModContainer.log.error("GL Error: " + errorString + "(" + error + ") @ " + desc);
+
+            error = GL11.glGetError();
         }
     }
 
@@ -107,7 +145,7 @@ public class FastTextureAtlasSprite extends TextureAtlasSprite {
 
     @Override
     public void generateMipmaps(int p_147963_1_) {
-        mipLevels = p_147963_1_;
+        mipLevels = AnimfixModContainer.maxUpdateMip >= 0 ? Math.min(AnimfixModContainer.maxUpdateMip, p_147963_1_) : p_147963_1_;
 
         super.generateMipmaps(p_147963_1_);
     }
